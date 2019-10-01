5661f6794de2d6bb87f9cf31cc06de7cff54a971
Adam <adam.vk@live.se>
2019-10-01 13:45:35 -0600

    working navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Home from './components/Home';
+import Home from './components/home/Home';
 import NoMatch from './components/NoMatch';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
@@ -10,19 +10,19 @@ import { Switch, Route, } from 'react-router-dom';
 import { Container, } from "semantic-ui-react";
 
 const App = () => (
-  <>
-    <Navbar />
-    <FetchUser>
-      <Container>
-        <Switch>
-          <ProtectedRoute exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-          <Route component={NoMatch} />
-        </Switch>
-      </Container>
-    </FetchUser>
-  </>
+    <>
+        <Navbar />
+        <FetchUser>
+        <Container>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
+                <Route component={NoMatch} />
+            </Switch>
+        </Container>
+        </FetchUser>
+    </>
 )
 
 export default App;
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Top from "./Top";
 import { Header, } from 'semantic-ui-react';
 
 const Home = () => (
-  <Header as="h3" textAlign="center">Devise Auth App</Header>
+    <Top />
 )
 
 export default Home;
